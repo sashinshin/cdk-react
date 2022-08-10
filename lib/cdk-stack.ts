1f450ed350243132b88b41f2ca92167b1b7df096
sasha <sasha.luhr@gmail.com>
2022-08-10 18:07:01 +0200

    init

---
+++ lib/cdk-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, 'ReactPipeline', {
+      pipelineName: 'ReactPipeline',
+      dockerEnabledForSynth: true,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('sashinshin/cdk-react', 'main'),
+        commands: [
+          'npm ci',
+          'cd react && npm i && cd ..',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+  }
+}
